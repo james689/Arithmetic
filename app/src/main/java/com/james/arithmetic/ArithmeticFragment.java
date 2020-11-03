12a9353535ae922aa372ae75a1746dccef829537
James <james689@users.noreply.github.com>
2020-11-03 17:41:06 +0000

    Added the BottomNavigationView to the ArithmeticFragment's layout

--- app/src/main/java/com/james/arithmetic/ArithmeticFragment.java
+++ app/src/main/java/com/james/arithmetic/ArithmeticFragment.java
@@ -1,5 +1,10 @@
 package com.james.arithmetic;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
 public class ArithmeticFragment extends Fragment {
@@ -7,4 +12,13 @@ public class ArithmeticFragment extends Fragment {
     public static ArithmeticFragment newInstance() {
         return new ArithmeticFragment();
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.arithmetic_fragment, container, false);
+
+        return view;
+    }
 }
+
