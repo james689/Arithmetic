cea8408e024a052ea19c8ad731744bc86b3cae4a
James <james689@users.noreply.github.com>
2020-11-04 18:51:09 +0000

    Added the four menu items to the BottomNavigationView and implemented an on click listener for them. Started building out more of the ArithmeticFragment GUI.

--- app/src/main/java/com/james/arithmetic/ArithmeticFragment.java
+++ app/src/main/java/com/james/arithmetic/ArithmeticFragment.java
@@ -1,13 +1,23 @@
 package com.james.arithmetic;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class ArithmeticFragment extends Fragment {
+
+    private static final String TAG = ArithmeticFragment.class.getSimpleName();
+    public enum ProblemType {ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION}
+
+    private ProblemType selectedProblemType = ProblemType.ADDITION;
 
     public static ArithmeticFragment newInstance() {
         return new ArithmeticFragment();
@@ -17,6 +27,33 @@ public class ArithmeticFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.arithmetic_fragment, container, false);
+        BottomNavigationView bottomNavigationView = view.findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setItemHorizontalTranslationEnabled(true);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_addition:
+                        Log.d(TAG, "addition selected");
+                        selectedProblemType = ProblemType.ADDITION;
+                        return true;
+                    case R.id.action_subtraction:
+                        Log.d(TAG, "subtraction selected");
+                        selectedProblemType = ProblemType.SUBTRACTION;
+                        return true;
+                    case R.id.action_multiplication:
+                        Log.d(TAG, "multiplication selected");
+                        selectedProblemType = ProblemType.MULTIPLICATION;
+                        return true;
+                    case R.id.action_division:
+                        Log.d(TAG, "division selected");
+                        selectedProblemType = ProblemType.DIVISION;
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
 
         return view;
     }
