e76de911425c9f44e62934d20a39e1e602122d1b
James <james689@users.noreply.github.com>
2020-11-08 18:41:50 +0000

    Implemented the behaviour for the show/hide answer button.

--- app/src/main/java/com/james/arithmetic/ArithmeticFragment.java
+++ app/src/main/java/com/james/arithmetic/ArithmeticFragment.java
@@ -6,29 +6,68 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.fragment.app.Fragment;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ArithmeticFragment extends Fragment {
 
-    private static final String TAG = ArithmeticFragment.class.getSimpleName();
+    public static final String TAG = ArithmeticFragment.class.getSimpleName();
+    public static final String KEY_ANSWER_VISIBLE = "answer_visible";
+
     public enum ProblemType {ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION}
 
     private ProblemType selectedProblemType = ProblemType.ADDITION;
+    private TextView answer;
+    private Button showAnswerButton;
+    private boolean answerVisible = false;
 
     public static ArithmeticFragment newInstance() {
         return new ArithmeticFragment();
     }
 
     @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // save whether the answer TextView is visible or not
+        outState.putBoolean(KEY_ANSWER_VISIBLE, answerVisible);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.arithmetic_fragment, container, false);
+
+        if (savedInstanceState != null) {
+            answerVisible = savedInstanceState.getBoolean(KEY_ANSWER_VISIBLE);
+        }
+        answer = view.findViewById(R.id.answer);
+        answer.setVisibility(answerVisible ? View.VISIBLE : View.GONE);
+
+        Button newQuestionButton = view.findViewById(R.id.new_question);
+        newQuestionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // generate a new question
+                Log.d(TAG, "new question button clicked");
+            }
+        });
+
+        showAnswerButton = view.findViewById(R.id.show_hide_answer);
+        updateShowAnswerButtonText();
+        showAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // show/hide the answer
+                Log.d(TAG, "show/hide answer button clicked");
+                answer.setVisibility(answerVisible ? View.GONE : View.VISIBLE);
+                answerVisible = !answerVisible;
+                updateShowAnswerButtonText();
+            }
+        });
+
         BottomNavigationView bottomNavigationView = view.findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setItemHorizontalTranslationEnabled(true);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
@@ -56,6 +95,11 @@ public class ArithmeticFragment extends Fragment {
         });
 
         return view;
+    }
+
+    // helper methods
+    private void updateShowAnswerButtonText() {
+        showAnswerButton.setText(answerVisible ? R.string.hide_answer : R.string.show_answer);
     }
 }
 
