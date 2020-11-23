3775e15debc6b592162117f0564fe84bda4418e5
James <james689@users.noreply.github.com>
2020-11-23 19:28:01 +0000

    Added an icon for the settings to the menu. Finished implementing the resetting preferences to defaults feature. Fixed the BottomNavigationView so that all labels are visible.

--- app/src/main/java/com/james/arithmetic/SettingsFragment.java
+++ app/src/main/java/com/james/arithmetic/SettingsFragment.java
@@ -1,11 +1,15 @@
 package com.james.arithmetic;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.preference.EditTextPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +21,24 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
+
+        Preference resetDefaultsBtn = findPreference(getString(R.string.key_reset_default_settings));
+        resetDefaultsBtn.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference arg0) {
+                // clear all of the preferences in SharedPreferences
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                preferences.edit().clear().commit();
+
+                // manually refresh/reload the preferences that are displayed on the screen since
+                // they won't be automatically refreshed otherwise.
+                //PreferenceManager.setDefaultValues(getActivity(), R.xml.preferences, true);
+                // see https://stackoverflow.com/questions/46655743/android-restore-applications-default-preferences
+                getPreferenceScreen().removeAll();
+                onCreatePreferences(null,null); // or onCreate(null) in your code
+                return true;
+            }
+        });
 
         // get hold of all of the EditText preferences
         List<EditTextPreference> editTextPreferences = new ArrayList<>();
@@ -41,6 +63,16 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         NumberInputPreference numberInputPreference = new NumberInputPreference();
         for (EditTextPreference preference : editTextPreferences) {
             preference.setOnBindEditTextListener(numberInputPreference);
+
+            /*preference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newVal) {
+                    final String value = (String) newVal;
+                    EditTextPreference etp = (EditTextPreference) preference;
+                    etp.setText(value);
+                    return true;
+                }
+            });*/
         }
     }
 
